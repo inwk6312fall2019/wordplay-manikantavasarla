732cce36bc19d123d4a93304ae96b29fa32b23ff
ChandanaPagolu <chandana.pagolu@dal.ca>
2019-09-21 19:32:28 -0700

    third

---
+++ thirdprogram.py
@@ -0,0 +1,20 @@
+fin = open('words.txt')
+
+def avoids(word,letter):
+    for char in word:
+        if char in letter:
+            return False
+    return True
+
+
+letter = input('What letters to exclude? ')
+count = 0
+for line in fin:
+    word = line.strip()
+    if avoids(word, letter):
+        count += 1
+        print(word)
+
+percent = (count / 113809.0) * 100
+
+print(str(percent) + "% of the words don't have " + letter + '.')
